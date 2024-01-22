5535ca4aef1fa8c40fd3c599e2bb405a83f0d3be
Sabari K <sabari8956@gmail.com>
2024-01-22 23:27:42 +0530

    wsgi/ manage [stock values]

--- sportshunt/wsgi.prod.py
+++ sportshunt/wsgi.prod.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportshunt.conf.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportshunt.conf.prod')
 
 application = get_wsgi_application()
