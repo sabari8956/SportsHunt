3a04c657019933bf42e5440618da3db543cbed12
Sabari K <sabari8956@gmail.com>
2024-01-23 19:47:00 +0530

    updated dev.py to use .env variables

--- sportshunt/conf/dev.py
+++ sportshunt/conf/dev.py
@@ -1,1 +1,9 @@
 from .common import *
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+DEBUG = os.environ.get("DEBUG")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(' ')
