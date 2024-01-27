3a5c935b1028c847df0bd4bdf5174e3e597a5f83
Sabari K <sabari8956@gmail.com>
2024-01-27 16:48:28 +0530

    Added Login And logout Feature

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import *
+
 urlpatterns = [
-    path("",index, name="index" ),
+    path("", index, name="index" ),
+    path("login/", login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 # Create your views here.
-def index(request):
-    return render(request, "core/index.html")
+def index(req):
+    return render(req, "core/index.html")
+
+def login_view(req):
+    if req.method == "POST":
+        username = req.POST["username"]
+        password = req.POST["password"]
+        user = authenticate(req, username=username, password=password)
+
+        if user is not None:
+            login(req, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(req, "auth/login.html", {
+                "message": "Invalid Username/ Password",
+        })
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
