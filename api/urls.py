d40c4ff8813277688bf34b4f331c284ad91ec213
Sabari K <sabari8956@gmail.com>
2024-03-15 17:11:20 +0530

    added cart api

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', OrganisationApi, name="org"),
+    path('add_to_cart/', add_to_cart, name="add_to_cart"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from organisers.models import *
 from core.models import *
 from .serializer import *
@@ -22,3 +23,12 @@ def OrganisationApi(req):
     serializers = OrganisationSerializer(data, many=True)
     # return Response(serializers.data)
     return redirect('core:index')
+
+@api_view(["GET","POST"])
+def add_to_cart(req):
+    if not req.user.is_authenticated:
+        return Response({"error": "You are not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
+    data = req.data
+    print(data)
+    messages.add_message(req, messages.WARNING, "added to cart successfully")
+    return Response({"message": data}, status=status.HTTP_200_OK)
